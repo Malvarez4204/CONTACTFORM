7e498839d68b73598c7b279deb2b6b6972bd6b44
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2022-07-18 20:40:00 -0500

    CONTACT FORM

---
+++ Contact Form/Contact Form/Contact_FormApp.swift
@@ -0,0 +1,16 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by MAC on 27/06/22.
+//
+
+import SwiftUI
+
+@main struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
